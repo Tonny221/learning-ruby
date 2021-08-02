5e8d53a566ffec7d5a17242e851c4204a88e315f
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2021-08-02 19:43:57 -0400

    basic variables presentation

---
+++ basics/variables.rb
@@ -0,0 +1,10 @@
+# basics variables to create a simple presentation!
+
+programmer_name = "Tonny"
+programmer_age = 17
+programmer_food = "Banana"
+
+
+puts ("Hi!, I'm " + programmer_name)
+puts ("I'm " + programmer_age.to_s)
+puts ("I like to eat " + programmer_food)
